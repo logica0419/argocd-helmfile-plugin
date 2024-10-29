8df02be9cf7fb1c1e9789cce0fd5465f1426e369
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-29 15:26:43 +0900

    fix helm wrapper

---
+++ component/helm-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# To enable the "lookup" function in the Helm template generation, we need to
+# set the "--dry-run" option to "server".
+# ref: https://github.com/argoproj/argo-cd/issues/5202#issuecomment-2040122017
+
+if
+  [[ ("$ENABLE_LOOKUP" == "true" || "$ARGO_CD_ENABLE_LOOKUP" == "true") &&
+    "$1" == "template" ]]
+then
+  /usr/local/bin/helm "$@" --dry-run=server
+else
+  /usr/local/bin/helm "$@"
+fi
