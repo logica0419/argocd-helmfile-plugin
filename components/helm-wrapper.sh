d34ed52f8257bcd4df90ff75cdd818391456e043
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-10-29 17:30:01 +0900

    fix helm wrapper

--- components/helm-wrapper.sh
+++ components/helm-wrapper.sh
@@ -5,7 +5,7 @@
 # ref: https://github.com/argoproj/argo-cd/issues/5202#issuecomment-2040122017
 
 if
-  [[ ("$ENABLE_LOOKUP" == "true" || "$ARGOCD_ENV_ENABLE_LOOKUP" == "true") &&
+  [[ ("$ENABLE_LOOKUP" != "" || "$ARGOCD_ENV_ENABLE_LOOKUP" != "") &&
     "$1" == "template" ]]
 then
   /usr/local/bin/helm "$@" --dry-run=server
